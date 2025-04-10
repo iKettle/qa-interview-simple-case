88d7f8d398528b986e15a9ee80cb493240e06f9c
iKettle <me.espirit@gmail.com>
2025-04-10 11:28:05 +0200

    style: cosmetic changes, moved base url

--- tests/login/LoginPage.ts
+++ tests/login/LoginPage.ts
@@ -1,37 +1,37 @@
 import { type Locator, type Page } from '@playwright/test';
 
 export class LoginPage {
-    readonly page: Page;
-    readonly emailField: Locator;
-    readonly passwordField: Locator;
-    readonly loginButton: Locator;
-    readonly signupLink: Locator;
-  
-    constructor(page: Page) {
-      this.page = page;
-      this.emailField = page.locator('#email');
-      this.passwordField = page.locator('#password');
-      this.loginButton = page.getByRole('button', {name: 'Login'});
-      this.signupLink = page.getByRole('link', {name: 'Signup'});
-    }
-  
-    async goto() {
-      await this.page.goto('localhost:8080/login');
-    }
-  
-    async inputEmail(email) {
-      await this.emailField.fill(email);
-    }
+  readonly page: Page;
+  readonly emailField: Locator;
+  readonly passwordField: Locator;
+  readonly loginButton: Locator;
+  readonly signupLink: Locator;
 
-    async inputPassword(password) {
-      await this.passwordField.fill(password);
-    }
-
-    async clickLogin() {
-      await this.loginButton.click();
-    }
-  
-    async clickSignupLink() {
-      await this.signupLink.click();
-    }
+  constructor(page: Page) {
+    this.page = page;
+    this.emailField = page.locator('#email');
+    this.passwordField = page.locator('#password');
+    this.loginButton = page.getByRole('button', {name: 'Login'});
+    this.signupLink = page.getByRole('link', {name: 'Signup'});
   }
+
+  async goto() {
+    await this.page.goto('/login');
+  }
+
+  async inputEmail(email) {
+    await this.emailField.fill(email);
+  }
+
+  async inputPassword(password) {
+    await this.passwordField.fill(password);
+  }
+
+  async clickLogin() {
+    await this.loginButton.click();
+  }
+  
+  async clickSignupLink() {
+    await this.signupLink.click();
+  }
+}
--- tests/login/login.test.ts
+++ tests/login/login.test.ts
@@ -1,35 +1,32 @@
-import { test, expect } from '@playwright/test'
-import { existingUsers } from '../../test-setup/localstorage.setup'
-import { LoginPage } from './LoginPage'
+import { test, expect } from '@playwright/test';
+import { existingUsers } from '../../test-setup/localstorage.setup';
+import { LoginPage } from './LoginPage';
 
-const existingUser = existingUsers[0]
-let  loginPage
+const existingUser = existingUsers[0];
+let  loginPage:LoginPage;
 
 test.describe('login form tests', () => {
   test.beforeEach(async ({page}) => {
-    loginPage = new LoginPage(page)
-    await loginPage.goto()
+    loginPage = new LoginPage(page);
+    await loginPage.goto();
   })
 
   test('logging in with existing account', async ({ page }) => {
-    const existingUser = existingUsers[0]
+    await loginPage.inputEmail(existingUser.email);
+    await loginPage.inputPassword(existingUser.password);
+    await loginPage.clickLogin();
 
-    await loginPage.inputEmail(existingUser.email)
-    await loginPage.inputPassword(existingUser.password)
-    await loginPage.clickLogin()
-
-    await expect(page.getByText(`Welcome ${existingUser.firstName} ${existingUser.lastName}`)).toBeVisible()
-    await expect(page.getByRole('button').getByText('Log out')).toBeVisible()
+    await expect(page.getByText(`Welcome ${existingUser.firstName} ${existingUser.lastName}`)).toBeVisible();
+    await expect(page.getByRole('button').getByText('Log out')).toBeVisible();
   })
 
   test('logging in with invalid credentials', async ({ page }) => {
+    await loginPage.inputEmail('testemail@test.com');
+    await loginPage.inputPassword('testpassword');
+    await loginPage.clickLogin();
 
-    await loginPage.inputEmail('testemail@test.com')
-    await loginPage.inputPassword('testpassword')
-    await loginPage.clickLogin()
-
-    await expect(page.getByText('Invalid credentials')).toBeVisible()
-    await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible()
+    await expect(page.getByText('Invalid credentials')).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
   })
 
 })
